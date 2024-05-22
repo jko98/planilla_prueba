153c5efda5f34ce2ba1a6a11c5e1bbaa53590585
Joaquin <maidanajoaquin10@gmail.com>
2024-05-22 20:42:02 -0300

    inicio validaciones

---
+++ validar.js
@@ -0,0 +1,12 @@
+function validacion(){
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var fecha = document.getElementById("fecha").value;
+    var genero = document.getElementById("genero").value;
+    var edad = document.getElementById("edad").value;
+    var direccion = document.getElementById("direccion").value;
+    var numerodire = document.getElementById("numero_dire").value;
+    var codigoArea = document.getElementById("cod_area").value;
+    var celular = document.getElementById("celu").value;
+    var email = document.getElementById("correo").value;
+ }
